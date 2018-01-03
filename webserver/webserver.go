1b681f0e45e23cc94a4169fc1d98e70548b6f379
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2018-01-03 12:19:55 -0500

    Added verbose logging to webserver

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"os"
 	"sync"
 
+	log "github.com/Sirupsen/logrus"
+	"github.com/alecthomas/kingpin"
 	"github.com/gurupras/minerconfig"
 )
 
+var (
+	app     = kingpin.New("minerconfig-webserver", "Miner webserver")
+	verbose = app.Flag("verbose", "Enable verbose messages").Short('v').Default("false").Bool()
+)
+
 func main() {
+	kingpin.MustParse(app.Parse(os.Args[1:]))
+
+	if *verbose {
+		log.SetLevel(log.DebugLevel)
+	}
+
 	minerconfig.RunServer("www", 61117)
 	wg := sync.WaitGroup{}
 	wg.Add(1)
